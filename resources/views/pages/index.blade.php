3033fc797afebea472840cb3e35232282fbcd3c9
yash-1018 <yash01np@gmail.com>
2024-02-07 16:03:31 +0530

    Admin can add new page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,15 +2,21 @@
     <div class="py-12 w-full">
         <div class="w-full mx-auto sm:px-6 lg:px-8">
             <div class="bg-white  overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
+                <div class="p-6 text-gray-900">
+                    <a href="{{ route('admin.pages.create') }}"><button class="mt-4 px-4 py-2 bg-blue-500 text-white rounded">New Page</button></a>
                     <h1 class="text-3xl font-bold mb-4">Pages</h1>
 
                     <ul class=" pl-4">
                         @foreach($types as $type)
-                        <li>
-                            <a href="{{ route('admin.pages.edit', ['id' => $type->id]) }}" class="text-blue-500 hover:underline text-xl">
+                        <li class="flex items-center">
+                            <a href="{{ route('admin.pages.edit', ['id' => $type->id]) }}" class="text-blue-500 hover:underline text-xl  flex-grow">
                                 {{ $type->type }}
                             </a>
+                            <form method="post" action="{{ route('admin.pages.destroy', ['id' => $type->id]) }}" class="inline-block">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="mt-2 px-3 py-1 bg-red-400 text-white rounded">Delete</button>
+                            </form>
                         </li>
                         @endforeach
                     </ul>
